c5495b6208b47f4327760275f97458e0caf3e843
Jameson Evans <6jh7ln@gmail.com>
2024-12-16 13:00:13 -0600

    alchemy history, balances working

--- src/alchemy/alchemy.service.ts
+++ src/alchemy/alchemy.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { Alchemy, AssetTransfersCategory, Network } from 'alchemy-sdk';
-
 @Injectable()
 export class AlchemyService {
-    private readonly client: Alchemy;
+  private readonly client: Alchemy;
+  // we need contract addresses to filter for uAsset contracts. For now, I've only implemented uXRP 0x2615a94df961278DcbC41Fb0a54fEc5f10a693aE To add others, just add their contract address to the list.
 
   constructor() {
     const config = {
       apiKey: process.env.ALCHEMY_API_KEY,
-      network: Network.ETH_MAINNET,
+      network: Network.BASE_MAINNET,
     };
     this.client = new Alchemy(config);
   }
 
-  async getHistory(toAddress: string){
+  async getHistory(toAddress: string) {
     return await this.client.core.getAssetTransfers({
-        fromBlock: "0x0",
-        fromAddress: "0x0000000000000000000000000000000000000000",
-        toAddress: toAddress,
-        excludeZeroValue: true,
-        category: [AssetTransfersCategory.ERC20],
-      });
+      toAddress: toAddress,
+      excludeZeroValue: true,
+      contractAddresses: ['0x2615a94df961278DcbC41Fb0a54fEc5f10a693aE'],
+      category: [AssetTransfersCategory.ERC20],
+    });
+    // do post processing on results e.g. check return type for errors vs <AssetTransfersResponse> and convert or drop rawBalance/decimals math, etc.
   }
-  
+
+  async getBalances(address: string) {
+    return await this.client.core.getTokensForOwner(address, {
+      contractAddresses: ['0x2615a94df961278DcbC41Fb0a54fEc5f10a693aE'],
+    });
+  }
 }
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AlchemyService } from './alchemy/alchemy.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly alchemyService: AlchemyService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly alchemyService: AlchemyService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get('orders')
-  getOrders() {
-    return this.alchemyService.getHistory('0x9B8Df6E244526ab5F6e6400d331DB28C8fdDdb55')
+  @Get('history/:address')
+  getOrders(@Param('address') address: string) {
+    return this.alchemyService.getHistory(address);
   }
 
+  @Get('balances/:address')
+  getBalances(@Param('address') address: string) {
+    return this.alchemyService.getBalances(address);
+  }
 }
